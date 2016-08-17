dc77e7b92227b2fd815001ab0143780a1eb0ddc1
Victor Genty <vgenty@nevis.columbia.edu>
2016-08-17 13:55:03 -0400

    add preliminary debug mode to develop

--- setup_laropencv.sh
+++ setup_laropencv.sh
@@ -7,13 +7,13 @@ if [ -z ${LARLITE_BASEDIR+x} ]; then
   return 
 fi
 
-
 export OPENCV_INCDIR=/usr/local/include
 export OPENCV_LIBDIR=/usr/local/lib
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$OPENCV_LIBDIR
 
 me="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 echo $me
+
 export LAROPENCV_BASEDIR=$me
 export LAROPENCV_IMAGECLUSTERDIR=$me/ImageCluster
 
