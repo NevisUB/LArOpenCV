ddb79935161ed5f2b8201ae961858e7eeea98163
Victor Genty <vgenty@nevis.columbia.edu>
2016-08-19 11:44:51 -0400

    can see hough lines now on check_user_info

--- ImageCluster/Filter/HoughPTrackShower.cxx
+++ ImageCluster/Filter/HoughPTrackShower.cxx
@@ -20,34 +20,80 @@ namespace larocv {
 						const ::cv::Mat& img,
 						larocv::ImageMeta& meta, larocv::ROI& roi)
   {
+
     
-    //Easiest way is to run hough lines on specific cluster
-    //rather than subset of ::cv::Mat img I think
+    // I thought about running hough line finder cluster-by-cluster but thats
+    // certainly not useful. I might as well do regression if that's the case.
+    // I think I would rather look for overall line structure in the (possibly) cropped image,
+    // this may help me idenfity clusters which are possibly connected to one another
 
-    std::vector<std::vector<std::array<int,4> > > houghs_v;
-    houghs_v.resize(clusters.size());
+    ::std::vector<::cv::Vec4i> lines;
+    ::cv::HoughLinesP(img,lines,_hough_rho,_pi/180.0,
+		      _hough_threshold,_hough_min_line_length,
+		      _hough_max_line_gap);
     
-    for(int i = 0; i < clusters.size(); ++i) {
-      
-      auto& cluster = clusters[i];
-      auto& houghs  = houghs_v[i];
-      
-      std::vector<::cv::Vec4i> lines;
+    //save the lines for now w/ debug mode
 
-      //works but subMat has different coordinates than img -.-
-      
-      auto& rect = cluster._boundingBox;
-      ::cv::Mat subMat(img, rect);
 
-      ::cv::HoughLinesP(subMat, lines, _hough_rho, _pi/180.0,
-      			_hough_threshold, _hough_min_line_length, _hough_max_line_gap);
+    /// Debug mode is off, return the result
+    if ( ! meta.debug() ) return clusters;
+    
+    //***************************************************
+    //***Debug Mode
+
+    std::stringstream ss1,ss2,ss3,ss4;
+
+    ::larlite::user_info uinfo{};
+    ss1 << "Algo_"<<Name()<<"_Plane_"<<meta.plane()<<"_clusters";
+    uinfo.store("ID",ss1.str());
+
+
+    ss1.str(std::string());
+    ss1.clear();
+
+    uinfo.store("N_lines",(int)lines.size());
+    
+    for(uint i=0;i<lines.size();++i) {
+
+      auto& line = lines[i];
+
+      ss1 << "line_" << i << "_x1";
+      ss2 << "line_" << i << "_y1";
+      ss3 << "line_" << i << "_x2";
+      ss4 << "line_" << i << "_y2";
       
-      houghs.resize(lines.size());
-      
-      for(int j = 0; j < lines.size(); ++j )
-      	houghs[j] = {{ lines[j][0],  lines[j][1], lines[j][2], lines[j][3]}}; //2 braces?
+      uinfo.store(ss1.str(),meta.XtoTimeTick(lines[i][0]));
+      uinfo.store(ss2.str(),meta.YtoWire    (lines[i][1]));
+      uinfo.store(ss3.str(),meta.XtoTimeTick(lines[i][2]));
+      uinfo.store(ss4.str(),meta.YtoWire    (lines[i][3]));
+      ss1.str(std::string()); ss1.clear();
+      ss2.str(std::string()); ss2.clear();
+      ss3.str(std::string()); ss3.clear();
+      ss4.str(std::string()); ss4.clear();
       
     }
+
+    meta.ev_user()->emplace_back(uinfo);
+    
+    // std::vector<std::vector<std::array<int,4> > > houghs_v;
+    // houghs_v.resize(clusters.size());
+    
+    // for(int i = 0; i < clusters.size(); ++i) {
+      
+    //   auto& cluster = clusters[i];
+      
+    //   //works... but subMat has different coordinates than img -.-
+      
+    //   auto& rect = cluster._boundingBox;
+    //   ::cv::Mat subMat(img, rect);
+
+    //   houghs.resize(lines.size());
+      
+    //   for(int j = 0; j < lines.size(); ++j )
+    //   	houghs[j] = {{ lines[j][0],  lines[j][1], lines[j][2], lines[j][3]}}; //2 braces?
+      
+    // }
+
     
     return clusters;
   }
