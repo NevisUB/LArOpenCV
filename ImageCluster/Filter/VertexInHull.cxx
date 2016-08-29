e9568a159c78ea32bf6b332d4e6cdf5eae8b0e91
Ariana Hackenburg <ariana.hackenburg@yale.edu>
2016-08-29 17:46:29 -0400

    Remove clusters with vertex in hull --ahack

---
+++ ImageCluster/Filter/VertexInHull.cxx
@@ -0,0 +1,51 @@
+#ifndef __VERTEXINHULL_CXX__
+#define __VERTEXINHULL_CXX__
+
+#include "VertexInHull.h"
+
+namespace larocv{
+
+  void VertexInHull::_Configure_(const ::fcllite::PSet &pset)
+  {}
+
+  Cluster2DArray_t VertexInHull::_Process_(const larocv::Cluster2DArray_t& clusters,
+					const ::cv::Mat& img,
+					larocv::ImageMeta& meta, larocv::ROI& roi)
+  { 
+
+    // Basically Vic's Convex hull alg.  
+    // Adds check on vertex in convex hull-- remove if true
+  
+    Cluster2DArray_t oclusters;
+    oclusters.reserve( clusters.size() );
+    
+    std::vector<std::vector<int> > hullpts_v;
+    hullpts_v.resize(clusters.size());
+
+    Point2D vtx_in_plane = roi.roivtx_in_image(meta); 
+    ::cv::Point2f vtx (vtx_in_plane.x,vtx_in_plane.y);
+    
+    for (unsigned i = 0; i < clusters.size(); ++i ) { 
+
+      auto& cluster = clusters [i];
+      auto& hullpts = hullpts_v[i];
+    
+      ::cv::convexHull(cluster._contour, hullpts);
+      hullpts.push_back(hullpts.at(0)); // odd that this is needed to close up the hull
+
+      std::vector<::cv::Point> hullcontour;
+      hullcontour.resize(hullpts.size()-1);
+
+      for(unsigned u=0;u<hullcontour.size();++u) 
+        hullcontour[u] = cluster._contour[ hullpts[u] ];
+
+      if( ::cv::pointPolygonTest(hullcontour,vtx,false) > 0 ) continue;
+      
+      oclusters.emplace_back(cluster);
+
+      }
+  
+  return oclusters; }
+
+}
+#endif
---
+++ ImageCluster/Filter/VertexInHull.h
@@ -0,0 +1,71 @@
+/**
+ * \file VertexInHull.h
+ *
+ * \ingroup ImageCluster
+ * 
+ * \brief Class def header for a class VertexInHull
+ *
+ * @author ariana hackenburg 
+ */
+
+/** \addtogroup ImageCluster
+
+    @{*/
+#ifndef __VERTEXINHULL_H__
+#define __VERTEXINHULL_H__
+
+#include "ClusterAlgoBase.h"
+#include "ClusterAlgoFactory.h"
+
+namespace larocv {
+  /**
+     \class VertexInHull
+     @brief A simple clustering algorithm meant to serve for testing/example by Kazu
+  */
+  class VertexInHull : public larocv::ClusterAlgoBase {
+    
+  public:
+    
+    /// Default constructor
+    VertexInHull(const std::string name="VertexInHull") : ClusterAlgoBase(name)
+    {}
+    
+    /// Default destructor
+    ~VertexInHull(){}
+
+    /// Finalize after (possily multiple) Process call. TFile may be used to write output.
+    void Finalize(TFile*) {}
+
+  protected:
+
+    /// Configuration method
+    void _Configure_(const ::fcllite::PSet &pset);
+
+    /// Process method
+    larocv::Cluster2DArray_t _Process_(const larocv::Cluster2DArray_t& clusters,
+				      const ::cv::Mat& img,
+				      larocv::ImageMeta& meta, larocv::ROI& roi);
+    
+  private:
+
+  };
+  
+  /**
+     \class larocv::VertexInHullFactory
+     \brief A concrete factory class for larocv::VertexInHull
+   */
+  class VertexInHullFactory : public ClusterAlgoFactoryBase {
+  public:
+    /// ctor
+    VertexInHullFactory() { ClusterAlgoFactory::get().add_factory("VertexInHull",this); }
+    /// dtor
+    ~VertexInHullFactory() {}
+    /// creation method
+    ClusterAlgoBase* create(const std::string instance_name) { return new VertexInHull(instance_name); }
+  };
+  /// Global larocv::VertexInHullFactory to register ClusterAlgoFactory
+  static VertexInHullFactory __global_VertexInHullFactory__;
+}
+#endif
+/** @} */ // end of doxygen group 
+
