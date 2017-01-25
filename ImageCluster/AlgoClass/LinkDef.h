986d75e3707d3613a0131c87c65478658eb86043
Victor Genty <vgenty@nevis.columbia.edu>
2017-01-25 12:32:00 -0500

    skeleton PCACrossing

--- ImageCluster/AlgoClass/LinkDef.h
+++ ImageCluster/AlgoClass/LinkDef.h
@@ -12,6 +12,7 @@
 
 #pragma link C++ class DefectBreaker+;
 #pragma link C++ class larocv::ClusterHIPMIP+;
+#pragma link C++ class larocv::PCACrossing+;
 #pragma link C++ class larocv::VertexParticleCluster+;
 
 //ADD_NEW_CLASS ... do not change this line
