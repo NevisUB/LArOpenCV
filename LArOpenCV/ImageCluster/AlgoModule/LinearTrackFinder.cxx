3c0fc9ab5cebcb9affe498128a0eeae776014f60
Rui <ran4@hawk.iit.edu>
2017-04-13 11:49:09 -0500

    adding comments

--- LArOpenCV/ImageCluster/AlgoModule/LinearTrackFinder.cxx
+++ LArOpenCV/ImageCluster/AlgoModule/LinearTrackFinder.cxx
@@ -17,16 +17,18 @@ namespace larocv {
 
   void LinearTrackFinder::_Configure_(const Config_t &pset)
   {
+    // Set Algo Verbosity
     _algo.set_verbosity(this->logger().level());
+    // Set Algo configuration
     _algo.Configure(pset.get<Config_t>("SingleLinearTrack"));
     
     auto algo_name_part = pset.get<std::string>("TrackVertexEstimate","");
 
     _algo_id_part = kINVALID_ALGO_ID;
-
+    // Check the producer exists
     if(!algo_name_part.empty())
       _algo_id_part = this->ID( algo_name_part );
-    
+    // Create container for this algorithm data
     Register(new data::LinearTrack3DArray);
   }
   
@@ -35,7 +37,9 @@ namespace larocv {
 
   void LinearTrackFinder::_Process_()
   {
+    // Read in images
     auto img_v = ImageArray();
+    // Set img parameters using LArPlaneGeo [the origin points, # cols, # rows etc]
     for(auto const& meta : MetaArray())
       _algo.SetPlaneInfo(meta);
     
@@ -49,18 +53,17 @@ namespace larocv {
 	  vtx2d_vv[plane].push_back(vtx3d.cvtx2d_v.at(plane).center);
 	}
       }
-
+      
       // Register to SingleLinearTrack algorithm to search for an independent (separated) single track
       for(size_t plane=0; plane<vtx2d_vv.size(); ++plane)
-	
 	_algo.RegisterVertex2D(plane,vtx2d_vv[plane]);
     }
     
     // Find linear track
     auto ltrack_v = _algo.FindLinearTrack(img_v);    
     LAROCV_DEBUG() << "Found linear track size " << ltrack_v.size() << std::endl;
-
-    // Record
+    
+    // Record 
     auto& data = AlgoData<larocv::data::LinearTrack3DArray>(0);
     for(size_t idx=0; idx<ltrack_v.size(); ++idx)
       data.emplace_back(std::move(ltrack_v[idx]));
