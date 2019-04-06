93b328fa2c203a51e63d889aafdc89923461cc76
Taritree <taritree.wongjirad@gmail.com>
2019-04-06 23:06:48 +0000

    added python3 compatibility

--- setup_laropencv.sh
+++ setup_laropencv.sh
@@ -21,3 +21,14 @@ export LAROPENCV_BASEDIR=$me
 export LAROPENCV_IMAGECLUSTERDIR=$me/ImageCluster
 export PYTHONPATH=$PYTHONPATH:$LAROPENCV_BASEDIR/python
 export PATH=${LAROPENCV_BASEDIR}/bin:${PATH}
+
+# Python version
+if [[ -z $LAROPENCV_PYTHON_VERSION ]]; then
+    export LAROPENCV_PYTHON_VERSION=`python -V 2>&1 | awk '{ print $2 }' | sed 's/\./\ /g' | awk '{ print $1 }'`
+    echo "LAROPENCV_PYTHON_VERSION =  ${LAROPENCV_PYTHON_VERSION}"
+    export LAROPENCV_PYTHON="python${LAROPENCV_PYTHON_VERSION}"
+else
+    echo "LAROPENCV_PYTHON_VERSION =  ${LAROPENCV_PYTHON_VERSION} (ALREADY SPECIFIED)"
+fi
+echo "LAROPENCV_PYTHON = ${LAROPENCV_PYTHON}"
+
